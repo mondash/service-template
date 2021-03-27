ffec1e8396f5ed6b34587bfc1a9feb2c59123d78
Matthew Ondash <ondashm@gmail.com>
2021-03-27 00:14:07 -0500

    Jest path aliasing and module resolution working now
    Updated example test

--- jest.config.ts
+++ jest.config.ts
@@ -1,26 +1,16 @@
 import { Config } from "@jest/types";
-import { pathsToModuleNameMapper } from "ts-jest/utils";
-import { compilerOptions } from "./tsconfig.json";
-
-const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
-  prefix: "<rootDir>/src/",
-});
-
-console.log({ moduleNameMapper });
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
+  modulePaths: ["<rootDir>/src"],
   setupFiles: ["./jest.setup.ts"],
   testEnvironment: "node",
   testPathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/build"],
   verbose: true,
 
-  moduleNameMapper,
-  modulePaths: ["<rootDir>/src/"],
-
   collectCoverage: true,
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-  coveragePathIgnorePatterns: ["__generated__"],
+  coveragePathIgnorePatterns: ["/__generated__/"],
 
   watchPathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/build"],
   watchPlugins: [
