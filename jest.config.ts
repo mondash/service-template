8f7fae7095e6c272a86dd889e4ae26ff895a200c
Matthew Ondash <ondashm@gmail.com>
2021-03-26 23:38:57 -0500

    More initial work and fixes
    No longer run prettier on generated files
    Attempt jest module aliasing (NOT WORKING YET)
    Renamed a few yarn scripts
    Updated tsconfigs
    Now uses webpack for builds (ALIASING WORKING)
    Fix endpoint log

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,12 @@
-import type { Config } from "@jest/types";
+import { Config } from "@jest/types";
+import { pathsToModuleNameMapper } from "ts-jest/utils";
+import { compilerOptions } from "./tsconfig.json";
+
+const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
+  prefix: "<rootDir>/src/",
+});
+
+console.log({ moduleNameMapper });
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
@@ -7,11 +15,8 @@ const config: Config.InitialOptions = {
   testPathIgnorePatterns: ["<rootDir>/node_modules", "<rootDir>/build"],
   verbose: true,
 
-  moduleNameMapper: {
-    "^types/(.+)": ["src/types/$1", "src/types/__generated__/$1"],
-    "^lib/(.+)": ["src/lib/$1"],
-    "^(.+)": ["src/$1", "$1"],
-  },
+  moduleNameMapper,
+  modulePaths: ["<rootDir>/src/"],
 
   collectCoverage: true,
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
